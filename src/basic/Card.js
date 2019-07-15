f3ca3d428712f0e90341ad05b384542253d72c19
Plaban J Rajkhowa <plabanj@geekyants.com>
2019-07-15 18:40:52 +0530

    Remove ListView

--- src/basic/Card.js
+++ src/basic/Card.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, ListView, ViewPropTypes } from 'react-native';
+import { View, ViewPropTypes } from 'react-native';
 import { connectStyle } from 'native-base-shoutem-theme';
 
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 
 class Card extends Component {
   render() {
-    if (this.props.dataArray && this.props.renderRow) {
-      const ds = new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2
-      });
-      const dataSource = ds.cloneWithRows(this.props.dataArray);
-      return (
-        <ListView
-          {...this.props}
-          enableEmptySections
-          dataSource={dataSource}
-          renderRow={this.props.renderRow}
-        />
-      );
-    }
     return (
       <View ref={c => (this._root = c)} {...this.props}>
         {this.props.children}
